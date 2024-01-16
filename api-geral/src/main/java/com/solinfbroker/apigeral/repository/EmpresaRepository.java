c66e21c7ded4a7028bb8fa426d35ce7209847ff6
Helter <helterpinheiro@gmail.com>
2024-01-16 09:31:37 -0300

    Modificacao de tipo date e ajuste de importacoes

--- api-geral/src/main/java/com/solinfbroker/apigeral/repository/EmpresaRepository.java
+++ api-geral/src/main/java/com/solinfbroker/apigeral/repository/EmpresaRepository.java
@@ -2,9 +2,9 @@ package com.solinfbroker.apigeral.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.solinfbroker.apigeral.model.Empresa;
+import com.solinfbroker.apigeral.model.EmpresaModel;
 
-public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+public interface EmpresaRepository extends JpaRepository<EmpresaModel, Long>{
 
   
 } 
