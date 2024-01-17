31eb4248c1655a944edd618373a9c7dba2976206
Helter <helterpinheiro@gmail.com>
2024-01-17 12:14:57 -0300

    Criacao CarteiraModel

---
+++ api-geral/src/main/java/com/solinfbroker/apigeral/model/CarteiraModel.java
@@ -0,0 +1,28 @@
+package com.solinfbroker.apigeral.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Table(name = "ativo")
+public class CarteiraModel {
+  @Column(name = "id_cliente", nullable = false)
+  private Long idCliente;
+
+  @Column(name = "id_ativo", nullable = false)
+  private Long idAtivo;
+
+  @Column(nullable = false)
+  private double quantidade;
+
+  @OneToOne
+  @JoinColumn(name = "id_cliente", referencedColumnName = "id", insertable = false, updatable = false)
+  private ClienteModel cliente;
+
+  @OneToMany
+  @JoinColumn(name = "id_ativo", referencedColumnName = "id", insertable = false, updatable = false)
+  private AtivoModel ativo;
+}
