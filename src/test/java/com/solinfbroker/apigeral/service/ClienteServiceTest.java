fe15e4938d5fdf4abbf1c3a1ba58cbfc0e8a7857
Michael <michael_cesar_sv@hotmail.com>
2024-01-19 18:39:35 -0300

    correção nos teste, add extenção do mockito

--- src/test/java/com/solinfbroker/apigeral/service/ClienteServiceTest.java
+++ src/test/java/com/solinfbroker/apigeral/service/ClienteServiceTest.java
@@ -3,8 +3,10 @@ package com.solinfbroker.apigeral.service;
 import com.solinfbroker.apigeral.model.ClienteModel;
 import com.solinfbroker.apigeral.repository.ClienteRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
@@ -12,7 +14,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class ClienteServiceTest {
 
