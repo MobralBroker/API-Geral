4b900623b85982c3006f51e2fa2dfbdb35a5298e
Michael <michael_cesar_sv@hotmail.com>
2024-01-19 18:13:43 -0300

    add tag de test para o properties

--- src/test/java/com/solinfbroker/apigeral/service/ClienteServiceTest.java
+++ src/test/java/com/solinfbroker/apigeral/service/ClienteServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClienteServiceTest {
 
     @Mock
