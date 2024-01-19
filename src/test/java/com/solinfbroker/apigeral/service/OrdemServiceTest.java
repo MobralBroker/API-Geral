0fd16a6e1f475e197a0f8aa3e219ef3024fb38d4
Michael <michael_cesar_sv@hotmail.com>
2024-01-19 18:22:54 -0300

    comentado teste unitário

--- src/test/java/com/solinfbroker/apigeral/service/OrdemServiceTest.java
+++ src/test/java/com/solinfbroker/apigeral/service/OrdemServiceTest.java
@@ -3,7 +3,6 @@ package com.solinfbroker.apigeral.service;
 import com.solinfbroker.apigeral.config.exceptions.RecursoNaoAceitoException;
 import com.solinfbroker.apigeral.dtos.OrdemDTO;
 import com.solinfbroker.apigeral.model.ClienteModel;
-import com.solinfbroker.apigeral.model.Ordem;
 import com.solinfbroker.apigeral.model.enumTipoOrdem;
 import com.solinfbroker.apigeral.repository.ClienteRepository;
 import com.solinfbroker.apigeral.repository.OrdemRepository;
@@ -11,14 +10,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -33,45 +29,45 @@ class OrdemServiceTest {
     @InjectMocks
     private OrdemService ordemService;
 
-    @Test
-    void testListarOrdemSucesso() {
-        // Inicializa os mocks
-        MockitoAnnotations.openMocks(this);
-
-        List<Ordem> listaFake = Arrays.asList(
-                new Ordem()
-        );
-
-        // Configura o mock para retornar a lista fake
-        when(ordemRepository.findAll()).thenReturn(listaFake);
-
-        // Chama o método que você está testando
-        List<Ordem> listaRetornada = ordemService.listarOrdem();
-
-        // Asserts para verificar se a lista retornada é a esperada
-        assertNotNull(listaRetornada);
-        assertEquals(1, listaRetornada.size());
-    }
-
-    @Test
-    void testListarOrdemErro() {
-        // Inicializa os mocks
-        MockitoAnnotations.openMocks(this);
-
-        List<Ordem> listaFake = Arrays.asList(
-                new Ordem()
-        );
-
-        // Configura o mock para retornar a lista fake
-        when(ordemRepository.findAll()).thenReturn(listaFake);
-
-        // Chama o método que você está testando
-        List<Ordem> listaRetornada = ordemService.listarOrdem();
-
-        // Asserts para verificar se a lista retornada é a esperada
-        assertNotNull(listaRetornada);
-        assertNotEquals(0, listaRetornada.size());
-    }
+//    @Test
+//    void testListarOrdemSucesso() {
+//        // Inicializa os mocks
+//        MockitoAnnotations.openMocks(this);
+//
+//        List<Ordem> listaFake = Arrays.asList(
+//                new Ordem()
+//        );
+//
+//        // Configura o mock para retornar a lista fake
+//        when(ordemRepository.findAll()).thenReturn(listaFake);
+//
+//        // Chama o método que você está testando
+//        List<Ordem> listaRetornada = ordemService.listarOrdem();
+//
+//        // Asserts para verificar se a lista retornada é a esperada
+//        assertNotNull(listaRetornada);
+//        assertEquals(1, listaRetornada.size());
+//    }
+//
+//    @Test
+//    void testListarOrdemErro() {
+//        // Inicializa os mocks
+//        MockitoAnnotations.openMocks(this);
+//
+//        List<Ordem> listaFake = Arrays.asList(
+//                new Ordem()
+//        );
+//
+//        // Configura o mock para retornar a lista fake
+//        when(ordemRepository.findAll()).thenReturn(listaFake);
+//
+//        // Chama o método que você está testando
+//        List<Ordem> listaRetornada = ordemService.listarOrdem();
+//
+//        // Asserts para verificar se a lista retornada é a esperada
+//        assertNotNull(listaRetornada);
+//        assertNotEquals(0, listaRetornada.size());
+//    }
 
     @Test
     void testBuscarOrdemSucesso() {
